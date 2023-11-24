7103c0aba12d7fb424bf0fca0c29bd68a7a45d2f
Eric Kersten <ekersten@gmail.com>
2023-11-24 12:07:27 -0300

    inicial

---
+++ lab/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-%k=!e+m+kldyh=q--$wv%-8t+wysae&nlt30=ut1=vby6zg$bs"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ webpack.mix.js
@@ -0,0 +1,3 @@
+let mix = require('laravel-mix');
+
+mix.js('chat/assets/js/chat/index.js', 'chat/static/chat/js/chat.js').react()
